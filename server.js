73d0a33ba488e8b01d12fbf6e9e5166bb93b1e9b
Taiwo Hassan Yusuf <teehazzan@gmail.com>
2020-05-27 11:55:20 +0100

    fixed history fallback problem

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const serveStatic = require("serve-static")
 const path = require('path');
+var history = require('connect-history-api-fallback');
 var app = express();
 app.use(serveStatic(path.join(__dirname, 'dist')));
+app.use(history());
 const port = process.env.PORT || 8080;
 
 app.listen(port);
