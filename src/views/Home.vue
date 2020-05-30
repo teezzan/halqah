001f23931ea098be47bfb529711224db5866563f
Taiwo Hassan Yusuf <teehazzan@gmail.com>
2020-05-30 09:23:45 +0100

    added some info to homepage

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,22 +1,24 @@
 <template>
   <div class="home">
+    <h1>Should contain basic info feed and A random lecture player</h1>
     <h1>{{ isLoggedIn }} and {{ authStatus }}</h1>
-    <xns-audio-player-simple :playlist="songs"></xns-audio-player-simple>
+    <div>
+      <xns-audio-player-simple :playlist="songs"></xns-audio-player-simple>
+    </div>
   </div>
 </template>
 
 <script>
-import { mapGetters } from 'vuex'
-
+import { mapGetters } from "vuex";
 
 // https://halqah.herokuapp.com/api/group/media/43c9020ea8b54dc02b3158d9111d96a1.ogg
 
 export default {
-  data () {
+  data() {
     return {
       songs: []
-    }
+    };
   },
-  computed: mapGetters(['isLoggedIn', 'authStatus'])
-}
+  computed: mapGetters(["isLoggedIn", "authStatus"])
+};
 </script>
