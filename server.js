4e81d7bce55967adfad2ce5cc956cfdbd7c71967
teezzan <teehazzan@gmail.com>
2020-05-30 04:00:52 +0100

    added fallback route to server.js

--- server.js
+++ server.js
@@ -3,8 +3,15 @@ const serveStatic = require("serve-static")
 const path = require('path');
 var history = require('connect-history-api-fallback');
 var app = express();
-app.use(serveStatic(path.join(__dirname, 'dist')));
 app.use(history());
+app.use(serveStatic(path.join(__dirname, 'dist')));
+
+app.get('*', function(req, res) {
+    res.redirect('/');
+});
+
+
+
 const port = process.env.PORT || 8080;
 
 app.listen(port);
