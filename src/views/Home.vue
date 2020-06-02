ac0b94021e34a6f99544098351bb76ee6977b5eb
Taiwo Hassan Yusuf <teehazzan@gmail.com>
2020-06-02 22:26:07 +0100

    rewrote main page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,13 @@
 <template>
   <div class="home">
-    <h1>Should contain basic info feed and A random lecture player</h1>
+    <h1>Welcome to HalqahMedia. We are under huge construction</h1>
     <h1>{{ isLoggedIn }} and {{ authStatus }}</h1>
-    <div>
-      <xns-audio-player-simple :playlist="songs"></xns-audio-player-simple>
-    </div>
+
   </div>
 </template>
 
 <script>
 import { mapGetters } from "vuex";
-
-// https://halqah.herokuapp.com/api/group/media/43c9020ea8b54dc02b3158d9111d96a1.ogg
 
 export default {
   data() {
