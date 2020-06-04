1a38af145d5da76687f528b261202b8865d0ab19
teezzan <teehazzan@gmail.com>
2020-06-05 00:33:32 +0100

    test no more

--- src/views/GroupList.vue
+++ src/views/GroupList.vue
@@ -7,7 +7,7 @@
       <b-input-group prepend="Find Channel" class="my-3">
         <b-form-input v-model="search" @keypress.esc="cancelModal(1)"></b-form-input>
       </b-input-group>
-<transition-group name="my-list" tag="ul" >
+
       <b-list-group-item
         v-for="(item, index) in searchResult"
         :key="index"
@@ -23,7 +23,7 @@
 
         <p class="mb-1 float-right">{{item.description}}</p>
       </b-list-group-item>
-</transition-group>
+
     </b-list-group>
     <!-- {{searchResult}} -->
   </b-container>
